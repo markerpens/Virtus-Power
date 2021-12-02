9dcbfcebdd35f136e2ba55d71dfd1b77fac7d789
Mark R <48663474+markerpens@users.noreply.github.com>
2021-12-01 22:02:56 -0800

    Details CSS Styling

--- php/details.php
+++ php/details.php
@@ -1,10 +1,17 @@
+<div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+        <h1 class="display-4">Virtus Power</h1>
+        <p class="lead">Lifter Details</p>
+    </div>
+</div>
+
 <?php
 // ----------- INCLUDE HEADER ----------- //
 include 'header.php';
 include 'main_functions.php';
 session_start();
 
-echo "<h1>Lifter Details</h1>";
+// echo "<h1>Lifter Details</h1>";
 
 // ----------- CONNECT TO DATABASE ----------- //
 
@@ -23,15 +30,24 @@ $query_string = "SELECT * FROM bcpa_powerlifting_database
 WHERE Id = '$id'";
 
 $query = mysqli_query($connection, $query_string);
+$query2 = mysqli_query($connection, $query_string);
 
-// start_table();
-
-// Loop through results
+// Lifter Details
+start_details_table();
 while ($row = mysqli_fetch_assoc($query)) {
-    details($row['Name'], $row['Sex'], $row['Division'], $row['Equipment'], $row['WeightClassKg'],  $row['Best3SquatKg'],  $row['Best3BenchKg'], $row['Best3DeadliftKg'], $row['Dots'], $row['TotalKg'],  $row['MeetName'],  $row['MeetTown'],  $row['Date']);
+    details($row['Name'], $row['Sex'], $row['Division'], $row['Equipment'], $row['WeightClassKg'],  $row['Best3SquatKg'],  $row['Best3BenchKg'], $row['Best3DeadliftKg'], $row['Dots'], $row['TotalKg']);
 }
+end_table();
 
-// end_table();
+// Meet Details
+start_meet_table();
+echo "<h2>Competition Details</h2>";
+while ($row = mysqli_fetch_assoc($query2)) {
+    meet_details($row['MeetName'],  $row['MeetTown'],  $row['Date']);
+}
+end_table();
+
+
 
 
 // ----------- INCLUDE FOOTER ----------- //
--- php/main_functions.php
+++ php/main_functions.php
@@ -34,22 +34,72 @@ function powerlifting_table($name, $seggs, $division, $equipment, $weightclass, 
 		</tr>";
 }
 
-// Create the details table on the details page
-function details($name, $seggs, $division, $equipment, $weightclass, $s, $b, $d, $total, $strcoefficient, $lastmeet, $lastmeetlocation, $lastmeetdate,)
+function start_details_table()
 {
-    echo "<h2>$name</h2>
-			<p>Sex: $seggs</p>
-			<p>Division: $division</p>
-			<p>Equipment: $equipment</p> 
-			<p>Weight Class: $weightclass</p>
-			<p>Best Squat: $s</p>
-            <p>Best Bench Press: $b</p>
-            <p>Best Deadlift: $d</p>
-            <p>Total: $total</p>
-            <p>Dots: $strcoefficient</p>
-            <p>Last Meet Name: $lastmeet</p>
-            <p>Last Meet Location: $lastmeetlocation</p>
-            <p>Last Meet Date: $lastmeetdate</p>";
+    echo "<table class=\" table table-light table-bordered\" border=\"1\">\n";
+    echo "<thead>
+    <th>Sex Division</th> 
+    <th>Age Division</th> 
+    <th>Equipment</th> 
+    <th>Weight Class (Kg)</th> 
+    <th>Best Squat (Kg)</th> 
+    <th>Best Bench Press (Kg)</th> 
+    <th>Best Deadlift (Kg)</th> 
+    <th>Total (Kg)</th> 
+    <th>Dots</th></tr>
+
+    </thead>";
+}
+
+// Create the lifter details table on the details page
+function details($name, $seggs, $division, $equipment, $weightclass, $s, $b, $d, $total, $strcoefficient)
+{
+    // echo "<h2>$name</h2>
+    // 		<p>Sex: $seggs</p>
+    // 		<p>Division: $division</p>
+    // 		<p>Equipment: $equipment</p> 
+    // 		<p>Weight Class: $weightclass</p>
+    // 		<p>Best Squat: $s</p>
+    //         <p>Best Bench Press: $b</p>
+    //         <p>Best Deadlift: $d</p>
+    //         <p>Total: $total</p>
+    //         <p>Dots: $strcoefficient</p>
+    //         <p>Last Meet Name: $lastmeet</p>
+    //         <p>Last Meet Location: $lastmeetlocation</p>
+    //         <p>Last Meet Date: $lastmeetdate</p>";
+
+    echo "<h2>$name</h2>";
+    echo "<tr";
+    echo "><td>$seggs</td>
+			<td>$division</td>
+			<td>$equipment</td> 
+			<td>$weightclass</td>
+			<td>$s</td>
+            <td>$b</td>
+            <td>$d</td>
+            <td>$total</td>
+            <td>$strcoefficient</td>
+		</tr>";
+}
+
+function start_meet_table()
+{
+    echo "<table class=\" table table-light table-bordered\" border=\"1\">\n";
+    echo "<thead>
+    <th>Meet Name</th> 
+    <th>Location</th> 
+    <th>Date</th></tr>
+    </thead>";
+}
+
+// Create the meet details table on the details page
+function meet_details($lastmeet, $lastmeetlocation, $lastmeetdate)
+{
+    echo "<tr";
+    echo "><td>$lastmeet</td>
+            <td>$lastmeetlocation</td>
+            <td>$lastmeetdate</td>
+		</tr>";
 }
 
 // Stop building the table
