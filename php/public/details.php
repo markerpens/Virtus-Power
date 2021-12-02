f2a9b9ebef1f99b90a03abf13ad7b77096432dac
Mark R <48663474+markerpens@users.noreply.github.com>
2021-12-01 22:09:13 -0800

    Fixed Details Page Glitch

--- php/public/details.php
+++ php/public/details.php
@@ -25,15 +25,24 @@ $query_string = "SELECT * FROM bcpa_powerlifting_database
 WHERE Id = '$id'";
 
 $query = mysqli_query($db, $query_string);
+$query2 = mysqli_query($db, $query_string);
 
-// start_table();
+start_table();
 
 // Loop through results
 while ($row = mysqli_fetch_assoc($query)) {
     details($row['Name'], $row['Sex'], $row['Division'], $row['Equipment'], $row['WeightClassKg'],  $row['Best3SquatKg'],  $row['Best3BenchKg'], $row['Best3DeadliftKg'], $row['Dots'], $row['TotalKg'],  $row['MeetName'],  $row['MeetTown'],  $row['Date']);
 }
 
-// end_table();
+end_table();
+
+// Meet Details
+start_meet_table();
+echo "<h2>Competition Details</h2>";
+while ($row = mysqli_fetch_assoc($query2)) {
+    meet_details($row['MeetName'],  $row['MeetTown'],  $row['Date']);
+}
+end_table();
 
 
 // ----------- INCLUDE FOOTER ----------- //
