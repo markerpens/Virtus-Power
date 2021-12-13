cb9651bd256f7f4c13ce082e83ec09fd2db3d856
Mark R <48663474+markerpens@users.noreply.github.com>
2021-12-12 19:15:16 -0800

    Rough Nav

--- php/private/header.php
+++ php/private/header.php
@@ -1,5 +1,7 @@
 <?php
-if(!isset($page_title)) { $page_title = 'Virtus Power'; }
+if (!isset($page_title)) {
+  $page_title = 'Virtus Power';
+}
 ?>
 
 
@@ -7,31 +9,64 @@ if(!isset($page_title)) { $page_title = 'Virtus Power'; }
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Virtus Power</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Virtus Power</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="../../css/main.css">
+  <link rel="stylesheet" type="text/css" href="../../css/main.css">
 </head>
 
 
 <navigation>
-    <ul>
-      <li>User: 
-        <?php
-            // checks if session is set, if it is then display the username
-            echo $_SESSION['username'] ?? '';
-          ?>
+  <ul>
+    <li>User:
+      <?php
+      // checks if session is set, if it is then display the username
+      echo $_SESSION['username'] ?? '';
+      ?>
+    </li>
+    <!-- link to the other pages that are used for login/register and logout  -->
+    <li><a href="<?php echo url_for('index.php'); ?>">Main</a></li>
+    <li><a href="<?php echo url_for('register.php'); ?>">Register</a></li>
+    <li><a href="<?php echo url_for('logout.php'); ?>">Logout</a></li>
+    <li><a href="<?php echo url_for('login.php'); ?>">Login</a></li>
+  </ul>
+</navigation>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">User: <?php
+                                          // checks if session is set, if it is then display the username
+                                          echo $_SESSION['username'] ?? '';
+                                          ?> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <!-- link to the other pages that are used for login/register and logout  -->
-      <li><a href="<?php echo url_for('index.php'); ?>">Main</a></li>
-      <li><a href="<?php echo url_for('register.php'); ?>">Register</a></li>
-      <li><a href="<?php echo url_for('logout.php'); ?>">Logout</a></li>
-      <li><a href="<?php echo url_for('login.php'); ?>">Login</a></li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
     </ul>
-  </navigation>
+  </div>
+</nav>
 
 <body>
